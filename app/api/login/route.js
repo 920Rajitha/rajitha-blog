9d3063fc94ad155ccb7c11ff8a4400220733b099
Rajitha Lakshan <165902491+920Rajitha@users.noreply.github.com>
2026-04-04 12:05:50 +0530

    logging update

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -3,7 +3,7 @@ export async function POST(req) {
 
   // 🔥 change this to your own
   const ADMIN_USER = "admin";
-  const ADMIN_PASS = "1234";
+  const ADMIN_PASS = "920@Rajitha";
 
   if (username === ADMIN_USER && password === ADMIN_PASS) {
     return Response.json({ success: true });
